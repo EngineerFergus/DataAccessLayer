87e67015d81ee4bf19c3d7933a23dd18cfc564e9
williamskei97 <williamskei97@gmail.com>
2022-05-14 19:14:48 -0400

    Began outlining tests for the DAL.

---
+++ Tests/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+using DataAccessLayer.Attributes;
+
+namespace Tests
+{
+    internal class Person : DBTable<Person>
+    {
+        [DBColumn("FirstName", 1)]
+        public string FirstName { get; set; }
+
+        [DBColumn("LastName", 2)]
+        public string LastName { get; set; }
+
+        [DBColumn("Age", 3)]
+        public int Age { get; set; }
+
+        [DBColumn("Weight", 4)]
+        public double Weight { get; set; }
+
+        public Person()
+        {
+            FirstName = string.Empty;
+            LastName = string.Empty;
+        }
+
+        public override void SetData(DbDataReader reader)
+        {
+            ID = (long)reader["ID"];
+            FirstName = (string)reader["FirstName"];
+            LastName = (string)reader["LastName"];
+            Age = (int)reader["Age"];
+            Weight = (double)reader["Weight"];
+        }
+
+        protected override string FormatUpdate()
+        {
+            return string.Format(UpdateString, ID, FirstName, LastName, Age, Weight);
+        }
+
+        protected override string FormatInsert()
+        {
+            return string.Format(InsertString, FirstName, LastName, Age, Weight);
+        }
+
+        protected override string FormatDelete()
+        {
+            return string.Format(DeleteString, ID);
+        }
+    }
+}
---
+++ Tests/SQLWriterTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class SQLWriterTests
+    {
+        private Person person = new Person();
+
+        [TestMethod]
+        public void WritesCreate()
+        {
+            string create = "CREATE TABLE IF NOT EXISTS Person(" +
+                ")";
+            Assert.AreEqual(create, person.GetCreate());
+        }
+
+        [TestMethod]
+        public void WritesRead()
+        {
+            string read = "This has not been written yet";
+            Assert.AreEqual(read, person.GetRead());
+        }
+
+        [TestMethod]
+        public void WritesInsert()
+        {
+            string insert = "This has not been written yet";
+            Assert.AreEqual(insert, person.GetInsert());
+        }
+
+        [TestMethod]
+        public void WritesUpdate()
+        {
+            string update = "Not written";
+            Assert.AreEqual(update, person.GetUpdate());
+        }
+
+        [TestMethod]
+        public void WritesDelete()
+        {
+            string delete = "not written";
+            Assert.AreEqual(delete, person.GetDelete());
+        }
+    }
+}
