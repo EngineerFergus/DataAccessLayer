56715792b0cefc370d026d69f39677110ed63c5c
williamskei97 <williamskei97@gmail.com>
2022-05-26 21:25:19 -0400

    added tests to esnure tables containing both a foreign key and a link to a foreign table object update their foreign key when parent key changes,

---
+++ Tests/UnitTests/TableState.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.UnitTests
+{
+    [TestClass]
+    public class TableState
+    {
+        [TestMethod]
+        public void UpdatesIDOnParentIDChanged()
+        {
+            Person person = new Person()
+            {
+                ID = 1
+            };
+
+            Dog dog = new Dog()
+            {
+                PersonID = 1,
+                ID = 1
+            };
+
+            person.Dogs.Add(dog);
+
+            person.ID = 2;
+
+            Assert.AreEqual(2, dog.PersonID);
+        }
+    }
+}
